744a251e726da010a91bf7fa4bdffdda743e97d7
JAEHO AHN <ajh4234@gmail.com>
2025-01-01 16:02:18 +0900

    Day 20-21: Snake Game- made Snake Class and Move Direction

--- Day 20-21 snake_game/main.py
+++ Day 20-21 snake_game/main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,20 +9,13 @@ screen.title("Snake Game")
 # 화면 갱신을 수동으로 설정
 screen.tracer(0)
 
-# 튜플로 반복문 돌리기
-starting_position = [(0,0), (-20,0), (-40,0)]
+snake = Snake()
 
-# 튜플을 만드는 이유가 무엇일까 ?
-turtle_list = []
-
-for position in starting_position:
-    new_turtle = Turtle("square")
-    new_turtle.color("white")
-    new_turtle.penup()
-    new_turtle.goto(position)
-    turtle_list.append(new_turtle)
-
-
+screen.listen()
+screen.onkey(snake.turn_up, "Up")
+screen.onkey(snake.turn_down, "Down")
+screen.onkey(snake.turn_left, "Left")
+screen.onkey(snake.turn_right, "Right")
 
 game_is_on = True  # 게임 루프를 제어하는 플래그 (True일 동안 게임이 계속 실행됨)
 
@@ -29,16 +23,6 @@ while game_is_on:
     screen.update()  # 화면을 갱신하여 모든 변경 사항을 즉시 반영
     time.sleep(0.1)  # 루프마다 0.1초 대기하여 게임 속도를 조절
 
-    # 터틀 리스트를 역순으로 반복 (뱀의 몸체를 뒤에서부터 처리)
-    for turtle in range(len(turtle_list) - 1, 0, -1):
-        # 앞 세그먼트(turtle_list[turtle - 1])의 x, y 좌표 가져오기
-        new_x = turtle_list[turtle - 1].xcor()
-        new_y = turtle_list[turtle - 1].ycor()
-
-        # 현재 세그먼트(turtle_list[turtle])를 앞 세그먼트 위치로 이동
-        turtle_list[turtle].goto(new_x, new_y)
-
-    turtle_list[0].forward(20)
-    turtle_list[0].left(90)
+    snake.move()
 
 screen.exitonclick()
---
+++ Day 20-21 snake_game/snake.py
@@ -0,0 +1,67 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+RIGHT = 0
+LEFT = 180
+UP = 90
+DOWN = 270
+
+class Snake:
+    def __init__(self):
+        """뱀의 초기 설정"""
+        # 뱀의 세그먼트 리스트
+        self.segments = []
+        # # 초기 위치들
+        # self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+        # 뱀 생성
+        self.create_snake()
+        # 뱀의 머리 설정
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        """뱀의 세그먼트를 생성하고 초기 위치에 배치"""
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        """새로운 세그먼트를 특정 위치에 추가"""
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def move(self):
+        """뱀을 앞으로 이동"""
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            # 앞 세그먼트의 위치를 현재 세그먼트로 복사
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        # 뱀의 머리를 앞으로 이동
+        self.head.forward(MOVE_DISTANCE)
+
+    def turn_left(self):
+        """뱀의 머리를 왼쪽으로 회전"""
+        # 현재 오른쪽으로 움직이는 경우 제외 반대쪽은 제외
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def turn_right(self):
+        """뱀의 머리를 오른쪽으로 회전"""
+        # 현재 왼쪽으로 움직이는 경우 제외 반대쪽은 제외
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def turn_up(self):
+        """뱀의 머리를 위쪽으로 회전"""
+        # 현재 아래로 움직이는 경우 제외 반대쪽은 제외
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def turn_down(self):
+        """뱀의 머리를 아래쪽으로 회전"""
+        # 현재 위로 움직이는 경우 제외 반대쪽은 제외
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
