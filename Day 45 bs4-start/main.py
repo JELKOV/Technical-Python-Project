5da8cd796c1dc48daf72e231f9e0e5964e3ebb36
JAEHO AHN <ajh4234@gmail.com>
2025-07-08 20:10:51 +0900

    update

--- Day 45 bs4-start/main.py
+++ Day 45 bs4-start/main.py
@@ -13,13 +13,13 @@ soup = BeautifulSoup(contents, 'html.parser')  # 'html.parser'는 HTML 데이터
 
 # ** Title 태그 정보 가져오기 **
 # <title> 태그 전체 출력
-print(soup.title)  # <title>Angela's Personal Site</title>
+print(soup.title)
 
 # <title> 태그 이름 가져오기
-print(soup.title.name)  # title
+print(soup.title.name)
 
 # <title> 태그의 텍스트 내용 가져오기
-print(soup.title.string)  # Angela's Personal Site
+print(soup.title.string)
 
 # ** Anchor 태그 정보 가져오기 **
 # <a> 태그를 모두 찾고 리스트로 반환
@@ -37,30 +37,30 @@ for tag in all_anchor_tags:
 # ** 특정 태그와 속성 기반 검색 **
 # id="name"을 가진 <h1> 태그 검색
 heading = soup.find(name='h1', id='name')
-print(heading)  # <h1 id="name">Angela Yu</h1>
+print(heading)
 
 # class="heading"을 가진 <h3> 태그 검색
 class_h3 = soup.find(name="h3", class_="heading")
-print(class_h3)  # <h3 class="heading">Books and Teaching</h3>
-print(class_h3.getText())  # Books and Teaching
+print(class_h3)
+print(class_h3.getText())
 
 # ** CSS 선택자 사용 예제 **
 # <p> 태그 안의 첫 번째 <a> 태그 선택
 company_url = soup.select_one("p a")
-print(company_url)  # <a href="https://www.appbrewery.co/">The App Brewery</a>
+print(company_url)
 
 # id="name"을 가진 태그 선택
 name = soup.select_one("#name")
-print(name)  # <h1 id="name">Angela Yu</h1>
+print(name)
 
 # class="heading"을 가진 모든 태그 선택
 heading_s = soup.select(".heading")
-print(heading_s)  # [<h3 class="heading">Books and Teaching</h3>, <h3 class="heading">Other Pages</h3>]
+print(heading_s)
 
 # ** Input 태그 정보 가져오기 **
 # <input> 태그 선택
 input_a = soup.find("input")
-print(input_a)  # <input type="text" name="q" maxlength="255" value="" />
+print(input_a)
 
 # <input> 태그의 maxlength 속성 값 가져오기
-print(input_a.get("maxlength"))  # 255
+print(input_a.get("maxlength"))
